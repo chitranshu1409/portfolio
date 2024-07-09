88fba4ad2764828f136db079e12705de42672186
chitranshu arya <chitranshu.arya9873@gmail.com>
2024-07-10 01:14:54 +0530

    commiting

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,6 @@ const Footer = () => {
         <a href="https://github.com/chitranshu1409" target='_blank'><FaGithub/></a>
         <a href="www.linkedin.com/in/chitranshu-arya-98562a248" target='_blank'><FaLinkedin/></a>
         <a href='mailTo:chitranshu.arya9873@gmail.com' target='_blank'><GrMail/></a>
-        <a href="https://leetcode.com/sahni1403/" target="_blank"><AiFillInstagram/></a>
       </div>
     </footer>
   )
