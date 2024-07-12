440d23953cbf4c6679072098c28fbdbc90d4b7d9
chitranshu arya <chitranshu.arya9873@gmail.com>
2024-07-12 13:16:11 +0530

    updated

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -7,8 +7,8 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
     
     Rooms:"A comprehensive video calling application designed to enhance productivity for remote teams by integrating multiple communication tools into a single platform. The application supports voice chat, video calls, real-time text chat, and screen sharing, providing seamless collaboration for team members across various locations.",
-    RoomsGithub:"https://github.com/chitranshu1409/frintend-room",
-    RoomsWebsite:"https://frintend-room.vercel.app/",
+    RoomsGithub:"https://github.com/chitranshu1409/room-frontend-latest",
+    RoomsWebsite:"https://room-frontend-latest.vercel.app/",
 
 
     Refer: "A full stack 'Refer & Earn' website that allows users to refer others by simply entering their email addresses. The system then automatically sends a referral email to the provided addresses, making it easy and efficient for users to share and earn rewards.",
